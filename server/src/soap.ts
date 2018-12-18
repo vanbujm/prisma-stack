8a78388ffb978da90a1aa35ad4c6f7d89bd40109
Ruan Beukes <ruan.coder@gmail.com>
2018-12-18 14:59:36 +0800

    first attempt at ts

--- server/src/soap.ts
+++ server/src/soap.ts
@@ -8,12 +8,51 @@ public WSDL file for testing which I found on StackOverflow:
 var url =
   'http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL';
 
-soap.createClient(url, (err, numberConversionClient) => {
+/*
+  soap for node
+  https://www.npmjs.com/package/soap
+*/
+
+var wsdlOptions = {
+  forceSoap12Headers: true,
+  envelopeKey: 'IEAEnvelope'
+};
+
+// let wsdlOptions = {
+//   overrideRootElement: {
+//     name: 'IEAEnvelope'
+//   }
+//   //xmlKey: 'IEAEnvelope'
+//   //overrideRootElement: {
+//   //name: 'IEAEnvelope'
+//   //   namespace: 'xmlns:tns',
+//   //   xmlnsAttributes: [
+//   //     {
+//   //       name: 'xmlns:xsi',
+//   //       value: 'http://www.w3.org/2001/XMLSchema-instance'
+//   //     },
+//   //     {
+//   //       name: 'xmlns',
+//   //       value:
+//   //         'http://www.auspost.com.au/Schema/AgencyServices/TrustBasedServices/IdentityEnrolmentApplication:v1'
+//   //     }
+//   //   ]
+//   //}
+// };
+
+soap.createClient(url, wsdlOptions, (err, numberConversionClient) => {
   let clientDescription = numberConversionClient.describe();
 
+  /*
   numberConversionClient.on('request', (xml, eid) => {
     console.log(`before request send: \r\n\t${eid}`);
     console.log(`xml: \r\n\t${xml}`);
+  });
+  */
+
+  numberConversionClient.on('soapError', (error, eid) => {
+    console.log(`soapError eid: \r\n\t${eid}`);
+    console.log(`soapError: \r\n\t${error}`);
   });
 
   var args = { ubiNum: 2 };
